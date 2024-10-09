d9cf139c1ef0de559072d57fb4f7b9df1b7ee5cc
Roman Prizov <aromawriter@mail.ru>
2024-10-09 11:37:48 +0300

    final project without versions

---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,7 @@
+import { Environment } from "./interface";
+
+export const environment: Environment = {
+  production: true,
+  apiKey: "AIzaSyBI3Eb0DSuAqwBK9_hIm0-Im5cuQmGwWCY",
+  fbDbUrl: "https://angular-blog-43528-default-rtdb.firebaseio.com",
+};
